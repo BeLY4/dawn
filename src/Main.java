92d3a4c614b558588695582628be8a64b4f73766
BeLY4 <gleb.osin.71@mail.ru>
2024-11-16 20:17:34 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 13676;
+        int bonus = 20;
+        int mile = price / bonus;
+        System.out.println("Стоимость билета: " + price + " руб");
+        System.out.println("Кол-во начисленных миль: " + mile + " миль");
+    }
+}
